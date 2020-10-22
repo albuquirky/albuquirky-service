3b7497b2bf04829e056e17fe7c14bc08c5688bf3
AugmenTab <tbaum92@live.com>
2020-10-22 13:29:58 -0600

    Initial commit.

---
+++ src/main/java/edu/cnm/deepdive/albuquirky/AlbuquirkyApplication.java
@@ -0,0 +1,13 @@
+package edu.cnm.deepdive.albuquirky;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlbuquirkyApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(AlbuquirkyApplication.class, args);
+  }
+
+}
---
+++ src/test/java/edu/cnm/deepdive/albuquirky/AlbuquirkyApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.cnm.deepdive.albuquirky;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlbuquirkyApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
