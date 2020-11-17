39ef5e2ba63e8f1d384bbca6bbba7fbdbf2345a6
AugmenTab <tbaum92@live.com>
2020-11-17 10:46:53 -0700

    Set up /profiles/me endpoint in ProfileController.

--- src/main/java/edu/cnm/deepdive/albuquirky/controller/ProductController.java
+++ src/main/java/edu/cnm/deepdive/albuquirky/controller/ProductController.java
@@ -13,4 +13,5 @@ public class ProductController {
     this.productService = productService;
 
   }
+
 }
--- src/main/java/edu/cnm/deepdive/albuquirky/controller/ProfileController.java
+++ src/main/java/edu/cnm/deepdive/albuquirky/controller/ProfileController.java
@@ -1,10 +1,17 @@
 package edu.cnm.deepdive.albuquirky.controller;
 
+import edu.cnm.deepdive.albuquirky.model.entity.Profile;
 import edu.cnm.deepdive.albuquirky.service.ProfileService;
+import org.springframework.hateoas.server.ExposesResourceFor;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/profiles")
+@ExposesResourceFor(Profile.class)
 public class ProfileController {
 
   private final ProfileService profileService;
@@ -12,4 +19,10 @@ public class ProfileController {
   public ProfileController(ProfileService profileService) {
     this.profileService = profileService;
   }
+
+  @GetMapping(value = "/me", produces = MediaType.APPLICATION_JSON_VALUE)
+  public Profile me(Authentication auth) {
+    return (Profile) auth.getPrincipal();
+  }
+
 }
