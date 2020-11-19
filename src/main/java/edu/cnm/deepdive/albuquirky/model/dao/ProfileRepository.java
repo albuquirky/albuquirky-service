cdd4da5c4e050869da16b26c30a5c7571045191f
John Jaramillo <jjaramillo1221@gmail.com>
2020-11-19 16:39:16 -0700

    javadocs attempted.

--- src/main/java/edu/cnm/deepdive/albuquirky/model/dao/ProfileRepository.java
+++ src/main/java/edu/cnm/deepdive/albuquirky/model/dao/ProfileRepository.java
@@ -10,8 +10,18 @@ import org.springframework.data.jpa.repository.JpaRepository;
  */
 public interface ProfileRepository extends JpaRepository<Profile, Long> {
 
+  /**
+   * The findFirstByUsername query finds a {@link Profile} by name
+   * @param name username of profile
+   * @return a {@link Profile} with the selected name
+   */
   Optional<Profile> findFirstByUsername(String name);
 
+  /**
+   * The findFirstByOauth query finds a {@link Profile} by uauth
+   * @param oauth authorization key for a profile
+   * @return a {@link Profile} with the selected oauth key
+   */
   Optional<Profile> findFirstByOauth(String oauth);
 
 }
